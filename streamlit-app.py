19bb8c45236df0883dc941ece58e731f23682b32
shamsksa <133244368+shamsksa@users.noreply.github.com>
2023-10-16 10:11:11 +0300

    Add files via upload

---
+++ streamlit-app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+st.header('st.Write')
+
+st.write('Hello, *World!* :football:')
+
+st.write(1234)
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
